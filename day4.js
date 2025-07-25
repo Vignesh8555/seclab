1daa4a56dcf16f733accfe8219c416ac6abf60fe
vignesh M <vigneshmurugaraj8555@gmail.com>
2025-07-25 10:43:47 +0530

    Add files via upload

---
+++ day4.js
@@ -0,0 +1,47 @@
+//let data={
+  //subject:"js",
+   // mark:80,
+
+    //minecraft:{
+      //  dirt:"sand",
+        //depth:-243,
+        
+    //},
+    //sum:function(){
+      //  let total=this.minecraft.dirt+this.minecraft.java;
+        //return total;
+    //}
+    
+//}
+//console.log(data.subject)
+//console.log(data["mark"])
+//console.log(data.minecraft.dirt)
+//console.log(data.minecraft.depth)
+
+
+//let person={
+ //  job:"tester",
+  // salary:12000
+//}
+//const {job:firstkey,salary:secondkey }=person
+//console.log(firstkey+" "+secondkey)
+
+
+//let array=new array(1,2,"three")
+//console.log(arr[2]);
+
+//let array2=["js",true];
+//console.log(array[1]);
+
+//let students=[
+  //  {neme: "kumar",grade: "B"}
+    //{name: "john",grade: "A"}
+//]
+// let gradestudents=students.filter(students => students.grade==="B")
+ //console.log(gradestudents);
+
+///sliece method
+
+let names=["kumar","arsath","fysal"];
+names.slice(1,1,"david");
+console.log(names)
