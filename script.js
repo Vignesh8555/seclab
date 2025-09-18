98380c7d38ed69aabd01162e111c5a3302c3b515
vignesh M <vigneshmurugaraj8555@gmail.com>
2025-09-18 14:14:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+function validatenum(phone){
+    const regex=/^(0-9)(10)$/
+    if(name.trim() =="" ){
+        return ("email cannot be empty")
+    }
+    if(!regex.text(email)){
+        return "Invalid Email Format"
+    }
+    return "email is valid"
+}
+
+function validatepassword(password){
+    const regex=/^()
+}
+function valideform(){
+    let phone=document.getElementById("phone",value)
+    let email=document.getElementById("email",value)
+    let name=document.getElementById("name",value)
+    let password=document.getElementById("password",value)
+
+    let phoneerror=validatenum("phone")
+    let emailerror=validatenum("email")
+    let nameerror=validatenum("name")
+    let passworderror=validatenum("password")
+
+      
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
